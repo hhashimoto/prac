4cba33f5d627f7ce09a5211670bfc4157ce1364f
hhashimoto <almite.bridge@gmail.com>
2019-07-13 15:04:51 +0900

    Immutable value definition

--- scala/prac/src/main/scala/example/Prac.scala
+++ scala/prac/src/main/scala/example/Prac.scala
@@ -5,4 +5,9 @@ object Prac extends App {
     println(1 + 1) // 2
     println("Hello!") // Hello!
     println("Hello," + " world!") // Hello, world!
+
+    val x = 1 + 1 // `val` => immutable value definition
+    println(x) // 2
+    val x2: Int = 1 + 1 // notice type Int
+    println(x2)  // 2
 }

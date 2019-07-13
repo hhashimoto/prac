6ca2b9bbb7007e75dc9e1fb04464c8184a274277
hhashimoto <almite.bridge@gmail.com>
2019-07-13 16:00:31 +0900

    Block scope

--- scala/prac/src/main/scala/example/Prac.scala
+++ scala/prac/src/main/scala/example/Prac.scala
@@ -6,13 +6,21 @@ object Prac extends App {
     println("Hello!") // Hello!
     println("Hello," + " world!") // Hello, world!
 
+    /* val - Immutable Value */
     val x = 1 + 1 // `val` => immutable value definition
     println(x) // 2
     val x2: Int = 1 + 1 // notice type Int
     println(x2)  // 2
     // x = 3 // compile error!
 
+    /* var - Mutable Value */
     var y = 1 + 1 // `var` => mutable value definition
     y = 3
     println(y * y) // 9
+
+    /* {} - Blocks */
+    println({
+        val x = 10
+        x * 2
+    }) // 20
 }

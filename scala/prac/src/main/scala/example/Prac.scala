53868c4bbaf4881b449247016c1e5306158382ed
hhashimoto <almite.bridge@gmail.com>
2019-07-14 13:40:05 +0900

    Tuples

--- scala/prac/src/main/scala/example/Prac.scala
+++ scala/prac/src/main/scala/example/Prac.scala
@@ -227,4 +227,28 @@ object Prac extends App {
     animals.append(dog)
     animals.append(cat)
     animals.foreach(pet => println(pet.name))
+
+    /* Tuples */
+    var ingredient = ("Sugar", 25)
+    println(ingredient) // (Sugar, 25)
+    println(ingredient._1) // Sugar
+    println(ingredient._2) // 25
+
+    val (name, quantity) = ingredient
+    println(name) // Sugar
+    println(quantity) // 25
+
+    val planets = List(
+        ("Mercury", 57.9), ("Venus", 108.2), ("Earth", 149.6),
+        ("Mars", 227.9), ("Jupiter", 778.3)
+    )
+    planets.foreach{
+        case ("Earth", distance) =>
+            println(s"Our planets is $distance million killometers from the sun")
+        case _ =>
+    }
+    val numPairs = List((2, 5), (3, -7), (20, 56))
+    for ((a, b) <- numPairs) {
+        println(a * b)
+    }
 }

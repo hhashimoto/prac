7e646ea1add820cd5f65fef11169a00f71ea3c5d
hhashimoto <almite.bridge@gmail.com>
2024-01-28 18:15:21 +0900

    TestResultを返すように変更

--- rust/echor/tests/cli.rs
+++ rust/echor/tests/cli.rs
@@ -2,18 +2,25 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
+// Boxはメモリをヒープに確保する
+// dynはError構造体のメソッドに対する呼び出しが動的ディスパッチされることを示す(理由は？？)
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> TestResult {
+    // ?はOk値のアンパックやErrの伝播に使う（らしい）
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    // returnキーワードの他、最終行に;なしで記述するとそれが返却される
+    Ok(())
 }
 
 #[test]
-fn hello1() {
-  let outfile = "tests/expected/hello1.txt";
-  let expected = fs::read_to_string(outfile).unwrap();
-  let mut cmd = Command::cargo_bin("echor").unwrap();
-  cmd.arg("Hello there").assert().success().stdout(expected);
+fn hello1() -> TestResult {
+    let expected = fs::read_to_string("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }

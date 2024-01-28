3b7bf6f422ca33989ec08aea74f1d9028225c8e7
hhashimoto <almite.bridge@gmail.com>
2024-01-28 15:53:02 +0900

    必須な位置引数(TEXT)とオプショナルなフラグ(-n)の定義
    やっていることはpretty-printしているだけ

--- rust/echor/src/main.rs
+++ rust/echor/src/main.rs
@@ -1,10 +1,25 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
     // '_'から始まる変数はunusedを明示するもので、コンパイラの警告を抑制できる
-    let _matches = App::new("echor")
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("hhashimoto <almite.bridge@gmail.com>")
         .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
         .get_matches();
+
+    println!("{:#?}", matches);
 }

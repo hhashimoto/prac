53df5fe09a5ad9459db2ff25a8e3192324a61451
hhashimoto <almite.bridge@gmail.com>
2024-01-28 15:40:08 +0900

    clap(コマンドライン引数解析やサブコマンドの定義ができるライブラリ)の導入
    * cargo run -- --help/-h
    標準的なヘルプの出力
    * cargo run -- --version/-V
    バージョンの出力

--- rust/echor/src/main.rs
+++ rust/echor/src/main.rs
@@ -1,3 +1,10 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    // '_'から始まる変数はunusedを明示するもので、コンパイラの警告を抑制できる
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("hhashimoto <almite.bridge@gmail.com>")
+        .about("Rust echo")
+        .get_matches();
 }

878b7df63e19e090a200d552805aa7dfeacd7747
hhashimoto <almite.bridge@gmail.com>
2024-02-03 23:12:38 +0900

    渡されたファイルのオープンとエラー処理に対応

--- rust/catr/src/lib.rs
+++ rust/catr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
@@ -51,7 +53,17 @@ pub fn get_args() -> MyResult<Config> {
 
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
-      println!("{}", filename);
+        match open(&filename) {
+            Err(err) => eprintln!("Failet to open {}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
     }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
